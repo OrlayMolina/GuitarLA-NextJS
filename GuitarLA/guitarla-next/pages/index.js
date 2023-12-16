b0bd02842b798c37280e9c697940f15711a46dfe
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-16 09:19:04 -0500

    se agrega la fuente outfit de google fonts.

--- GuitarLA/guitarla-next/pages/index.js
+++ GuitarLA/guitarla-next/pages/index.js
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <h1>Hola mundo en next</h1>
-  )
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap" rel="stylesheet"/>
+      </Head>
+      <h1>Hola mundo en next</h1>
+    </>
+  );
 }
