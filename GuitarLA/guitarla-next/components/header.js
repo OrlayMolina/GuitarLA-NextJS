994b44296de8c57a472a07c67db69315a72e541b
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-16 12:38:15 -0500

    Se agregan estilos al header.

--- GuitarLA/guitarla-next/components/header.js
+++ GuitarLA/guitarla-next/components/header.js
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from '../styles/header.module.css';
 
 export default function Header() {
 
     return (
-        <header>
-            <div className="contenedor">
+        <header className={styles.header}>
+            <div className={`contenedor ${styles.barra}`}>
                 <Image src="/img/logo.svg" width={300} height={40} alt='imagen logotipo' />
 
-                <nav>
+                <nav className={styles.navegacion}>
 
                     <Link href='/'>Home</Link>
                     <Link href='/about'>About Us</Link>
