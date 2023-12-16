9bf45c6ed4c86a46d8ea9022a4012dd6fb431276
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-16 10:40:11 -0500

    se emplea el routing de Next similar al react router dom.

--- GuitarLA/guitarla-next/pages/index.js
+++ GuitarLA/guitarla-next/pages/index.js
@@ -1,14 +1,11 @@
-import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap" rel="stylesheet"/>
-      </Head>
       <h1>Hola mundo en next</h1>
+
+      <Link href='/nosotros'>Nosotros</Link>
     </>
   );
 }
---
+++ GuitarLA/guitarla-next/pages/nosotros.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Nosotros() {
+  return (
+    <div>
+        <h1>Nosotros</h1>
+        <Link href='/'>A Inicio</Link>
+    </div>
+  )
+}
