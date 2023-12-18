46f3500611dd190ba40562741aa590df5c649140
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-17 21:03:22 -0500

    Se crean los estilo del footer.

--- GuitarLA/guitarla-next/components/footer.js
+++ GuitarLA/guitarla-next/components/footer.js
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+import styles from '../styles/footer.module.css';
+
 export default function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className={styles.footer}>
+            <div className={`contenido ${styles.contenido}`}>
+                <nav className={styles.navegacion}>
+
+                    <Link href='/'>Home</Link>
+                    <Link href='/about'>About Us</Link>
+                    <Link href='/blog'>Blog</Link>
+                    <Link href='/store'>Store</Link>
+
+                </nav>
+
+                <p className={styles.copyright}>
+                    Todos los derechos reservados {new Date().getFullYear()}
+                </p>
+            </div>
+        </footer>
     )
 }
